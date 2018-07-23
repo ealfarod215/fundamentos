c0dbb9ac4dcc654553da1c8c7faf9d111503c6b2
Edison David Alfaro Duarte <39379336+ealfarod215@users.noreply.github.com>
2018-07-23 13:35:46 -0600

    trabajo en clase semana 11

---
+++ inclass/semana-11/Personas/src/personas/Logica.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personas;
+
+/**
+ *
+ * @author Edison
+ */
+public class Logica {
+    private String Name;
+    private String Edad;
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public String getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+    
+}
