4362209d526ce3bca01fe42bca538dd31e5c0767
Edison David Alfaro Duarte <39379336+ealfarod215@users.noreply.github.com>
2018-08-04 21:58:59 -0600

    HW-Login exercise

---
+++ extraclass/08-12-Login/User_Login/src/user_login/User_Login.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user_login;
+
+/**
+ *
+ * @author Edison
+ */
+public class User_Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    private String User;
+    private String Password;
+
+
+    /**
+     * @return the User
+     */
+    public String getUser() {
+        return User;
+    }
+
+    /**
+     * @param User the User to set
+     */
+    public void setUser(String User) {
+        this.User = User;
+    }
+
+    /**
+     * @return the Password
+     */
+    public String getPassword() {
+        return Password;
+    }
+
+    /**
+     * @param Password the Password to set
+     */
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+
+   
+    
+}
